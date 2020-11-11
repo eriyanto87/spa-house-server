76589c23da92877c2203047c1c3904438fcc3e4b
Evisari Riyanto <evisari111012@gmail.com>
2020-11-11 10:53:53 -0800

    heroku update

--- src/config.js
+++ src/config.js
@@ -3,4 +3,6 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL:
     process.env.DATABASE_URL || "postgresql://evi@localhost/spa-house",
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL || "postgresql://evi@localhost/spa-house",
 };
