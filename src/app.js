c116e3c7ce319ac40800fac892885141c4efd4ae
Evisari Riyanto <evisari111012@gmail.com>
2020-11-23 12:17:27 -0800

    fixed product to production

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.get("/", (req, res) => {
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
-  if (NODE_ENV === "product") {
+  if (NODE_ENV === "production") {
     response = { error: { message: "server error" } };
   } else {
     console.log(error);
